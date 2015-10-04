61354fae029333207ef258f76856cab723f0a7b8
anantsrivast <anant.sriv@berkeley.edu>
2015-10-03 20:40:52 -0500

    commit

--- SearchEngine-VineetGangwar-AnantSrivastava/Hybrid System Code/Hybrid_System_IndexMapper.py
+++ SearchEngine-VineetGangwar-AnantSrivastava/Hybrid System Code/Hybrid_System_IndexMapper.py
@@ -16,7 +16,7 @@ from pymongo import MongoClient
 
 class IndexMapperNew:
     def __init__(self):
-        conn=boto.s3.connection.S3Connection('tesr')
+        conn=boto.s3.connection.S3Connection('tes   r')
         self.bucket=conn.get_bucket('srs-mids-anant5')
         
         
